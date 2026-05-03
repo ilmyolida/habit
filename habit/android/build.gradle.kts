ff35445bec42fd566555a7e868feb1fc3522dfa2
muhammadsodik <ilmyolida77@gmail.com>
2026-05-04 04:52:05 +0600

    habit

--- habit/android/build.gradle.kts
+++ habit/android/build.gradle.kts
@@ -3,9 +3,7 @@
 plugins {
     id("com.android.application") version "7.3.0" apply false
     kotlin("android") version "1.9.0" apply false
-    
-    // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 allprojects {
