ccc226f4d7425173f7b4fbb2afa1044c226f3542
muhammadsodik <ilmyolida77@gmail.com>
2026-05-03 21:33:11 +0600

    hhabit

--- habit/android/settings.gradle.kts
+++ habit/android/settings.gradle.kts
@@ -1,18 +1,14 @@
-pluginManagement {
-    // 🔥 TUZATILDI: Flutter SDK path olish
-    def flutterSdkPath = {
-        def properties = new Properties()
-        def localPropertiesFile = file("local.properties")
-        if (localPropertiesFile.exists()) {
-            localPropertiesFile.withInputStream { properties.load(it) }
-        }
-        def flutterSdkPath = properties.getProperty("flutter.sdk")
-        assert flutterSdkPath != null, "flutter.sdk not set in local.properties"
-        return flutterSdkPath
-    }()
-    
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+import java.util.Properties
 
+fun properties(file: File): Properties {
+    return Properties().apply {
+        if (file.exists()) {
+            file.inputStream().use { load(it) }
+        }
+    }
+}
+
+pluginManagement {
     repositories {
         google()
         mavenCentral()
@@ -20,11 +16,17 @@ pluginManagement {
     }
 }
 
+val flutterSdkPath = requireNotNull(properties(file("local.properties")).getProperty("flutter.sdk")) {
+    "flutter.sdk not set in local.properties"
+}
+
+includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "7.3.0" apply false  // 🔥 TUZATILDI: 8.11.1 -> 7.3.0 (Flutter bilan mos)
     // START: FlutterFire Configuration
-    id("com.google.gms.google-services") version("4.3.15") apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
     // END: FlutterFire Configuration
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false  // 🔥 TUZATILDI: 2.2.20 -> 1.9.0
 }
