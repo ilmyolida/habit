ff2f0483d3cf796c984a41f89b567dbf8bf1b93e
muhammadsodik <ilmyolida77@gmail.com>
2026-05-04 05:26:49 +0600

    habit

--- habit/android/app/build.gradle.kts
+++ habit/android/app/build.gradle.kts
@@ -47,7 +47,7 @@ flutter {
 
 // 🔥 TUZATILDI: Dependencies to'g'ri joyga qo'yildi
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.0")
     implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
--- habit/android/settings.gradle.kts
+++ habit/android/settings.gradle.kts
@@ -20,11 +20,11 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.1.1" apply false  // 🔥 TUZATILDI: Flutter 3.41.9 minimum version
+    id("com.android.application") version "8.6.0" apply false  // 🔥 TUZATILDI: Flutter 3.41.9 recommended version
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version "4.3.15" apply false
     // END: FlutterFire Configuration
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false  // 🔥 TUZATILDI: 2.2.20 -> 1.9.0
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false  // 🔥 TUZATILDI: Flutter recommended Kotlin version
 }
 
 include(":app")
